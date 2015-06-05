67f59c9900defbf98df2e4c0147283f5fb099dae
layingback <laying.back@layingback.net>
2015-06-05 18:47:53 +0200

    Edit cpg_inst.php to change \"\" to \'\' to fix paraser error on Update

--- Pro_News/modules/Pro_News/cpg_inst.php
+++ Pro_News/modules/Pro_News/cpg_inst.php
@@ -476,7 +476,7 @@ eval("
 				}
 
 				if (\$prev_version <= '4.0.1') {
-					\$installer->add_query('ADD', 'pronews_articles', \"'seod varchar(255) NOT NULL default \"\"'\");
+					\$installer->add_query('ADD', 'pronews_articles', \"'seod varchar(255) NOT NULL default \'\''\");
 					\$installer->add_query('INSERT', 'config_custom', \"'pro_news', 'aspect', '0'\");
 					\$installer->add_query('INSERT', 'config_custom', \"'pro_news', 'opn_grph', '0'\");
 				}
