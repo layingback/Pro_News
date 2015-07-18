fffd14758cf77e9adbc56badf44fc9ac209452c7
layingback <laying.back@layingback.net>
2015-07-18 15:29:02 +0200

    4.0.5 version number +

--- Pro_News/modules/Pro_News/cpg_inst.php
+++ Pro_News/modules/Pro_News/cpg_inst.php
@@ -29,7 +29,7 @@ eval("
 		// class constructor
 			function $mod_dirname() {
 				\$this->radmin = true;
-				\$this->version = '4.0.4';
+				\$this->version = '4.0.5';
 				\$this->modname = $mod_dirname;
 				\$this->description = 'Pro_News CM&#8482; - Ultra Configurable Content Management - interfaces to Forums,  Photo Gallery & Calendar &nbsp; (Ver 4: developed and extended from original beta module of same name by Kauragari)';
 				\$this->author = 'layingback';
